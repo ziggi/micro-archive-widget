1de6624aa53fce49628767d92fb58857f2b4a2b2
Sergei Marochkin <me@ziggi.org>
2016-03-24 10:14:21 +0300

    Fix working in non-chrome browsers

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-(function() {
-	var clickElements = document.querySelectorAll('li.archive-micro-year');
-	var blockElements = document.querySelectorAll('li.archive-micro-year ul');
+document.addEventListener('DOMContentLoaded', function() {
+	var clickElements = Array.from(document.querySelectorAll('li.archive-micro-year'));
+	var blockElements = Array.from(document.querySelectorAll('li.archive-micro-year ul'));
 
 	clickElements.forEach(function(clickElement) {
 		clickElement.querySelector('a').addEventListener('click', function() {
@@ -15,4 +15,4 @@
 			});
 		});
 	});
-})();
+});
